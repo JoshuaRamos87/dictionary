3ca7e782a3e6d7bdf7a93713d1b255bf25780f9d
Josh <joshua87@usf.edu>
2021-07-03 16:43:40 -0400

    added discord bot interface and synonym command

--- test.js
+++ test.js
@@ -1,47 +1,116 @@
 
-const ps = require("prompt-sync")
-let http = require("https");
-const prompt = ps();
-let word = prompt("type word for definition ");
+const mySecret = process.env['TOKEN']
+const Discord = require("discord.js");
+const http = require("https");
+const client = new Discord.Client();
 
 
-let options = {
-    "method": "GET",
-    "hostname": "api.dictionaryapi.dev",
-    "path": '/api/v2/entries/en_US/',
-    "headers": 
-    {
-        'custom': 'Custom Header Demo works'
+client.on("ready", () => {
+  console.log(`Logged in as
+  ${client.user.tag}!`)
+});
+client.on("message", msg => {
+    
+    if(msg.author.bot) return;
+
+    try{
+      if(msg.toString().includes("$define"))
+      {
+        let word ="";
+        for(let i = 1; msg.toString().split(" ")[i] !== undefined; ++i)
+          word = word + msg.toString().split(" ")[i].toLowerCase() + "%20";
+      
+        console.log(word)
+
+        findWord(word,msg,1)
+        word = "";
+      }
+      else if(msg.toString().includes("$synonym"))
+      {
+        let word ="";
+        for(let i = 1; msg.toString().split(" ")[i] !== undefined; ++i)
+          word = word + msg.toString().split(" ")[i].toLowerCase() + "%20";
+
+        console.log(word)
+
+        findWord(word,msg,2)
+        word = "";
+      }
+      else if(msg.toString().includes("$help"))
+      {
+         msg.channel.send(`$define word`);
+         msg.channel.send(`$synonym word`);
+
+      }
     }
-};
-options["path"] += word
+    catch(err){}
 
-let req = http.request(options, function (res) {
-let chunks = [];
-
-res.on("data", function (chunk) {
-    chunks.push(chunk);
 });
+client.login(mySecret);
 
-res.on("end", function () {
-        let body = Buffer.concat(chunks);
-        let jsonObject = JSON.parse(body.toString())  
-        
-        try{
+
+function displaySyn(jsonObject,msg)
+{
+            for(let l = 0; l < Object.keys(jsonObject).length; l++)
+              for(let i = 0; i < Object.keys(jsonObject[l]["meanings"]).length; i++)
+                  for(let j = 0; j < Object.keys(jsonObject[l]["meanings"][i]["definitions"]).length; j++)
+                    for(let n = 0; n < Object.keys(jsonObject[l]["meanings"][i]["definitions"][j]["synonyms"]).length; n++)
+                      msg.channel.send("- " + jsonObject[l]["meanings"][i]["definitions"][j]["synonyms"][n]);
+}
+
+function displayDef(jsonObject,msg)
+{
+  for(let l = 0; l < Object.keys(jsonObject).length; l++)
+              for(let i = 0; i < Object.keys(jsonObject[l]["meanings"]).length; i++)
+                  for(let j = 0; j < Object.keys(jsonObject[l]["meanings"][i]["definitions"]).length; j++)
+                      msg.channel.send("- " + jsonObject[l]["meanings"][i]["definitions"][j]["definition"])
+
+}
+
+
+function findWord(word,msg,choice)
+{
+
+  let options = {
+      "method": "GET",
+      "hostname": "api.dictionaryapi.dev",
+      "path": '/api/v2/entries/en_US/',
+      "headers": 
+      {
+          'custom': 'Custom Header Demo works'
+      }
+  };
+  options["path"] += word
+
+  let req = http.request(options, function (res) {
+  let chunks = [];
+
+  res.on("data", function (chunk) {
+      chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+          let body = Buffer.concat(chunks);
+          let jsonObject = JSON.parse(body.toString())  
+
+          try{
+              
+              if(jsonObject["title"] === "No Definitions Found"){
+                  msg.channel.send(jsonObject["title"])
+                  return
+              }
+              
+
+          switch(choice)
+          {
+            case 1: displayDef(jsonObject,msg); break;
+            case 2: displaySyn(jsonObject,msg); break;
             
-            if(jsonObject["title"] === "No Definitions Found")
-            {
-                console.log(jsonObject["title"])
-                process.exit()
-            }
-            } catch(err){}
+          }
+          } catch(err){}
+      });
 
-        for(let l = 0; l < Object.keys(jsonObject).length; l++)
-            for(let i = 0; i < Object.keys(jsonObject[l]["meanings"]).length; i++)//which meaning im on
-                for(let j = 0; j < Object.keys(jsonObject[l]["meanings"][i]["definitions"]).length; j++)//which def im on
-                    console.log(jsonObject[l]["meanings"][i]["definitions"][j]["definition"])
+  });
 
-    });
-});
-
-req.end();
+  req.end();
+}
